28ed007baa28bd0e93e993f611507e4568910e26
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-30 02:13:43 +0800

    start practice

---
+++ script.js
@@ -0,0 +1,8 @@
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', () => {
+    console.log('it was clicked')
+});
