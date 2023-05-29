bf01da1074d896fb7acc35d6117d9de799cc4af1
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-30 03:05:13 +0800

    typescript define basic function

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello typescript hello!');
+var inputNum1 = document.getElementById('input1');
+var inputNum2 = document.getElementById('input2');
+var submitBtn = document.querySelector('button');
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener('click', function () {
+    console.log("The sum ".concat(addNumbers(+inputNum1.value, +inputNum2.value)));
+});
