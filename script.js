1c2cbd218406e24674a851a30941e931dd0cdcfc
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-30 02:34:55 +0800

    typescript added

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const input2 = document.getElementById('input2');
 const button = document.querySelector('button');
 
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+
 button.addEventListener('click', () => {
-    console.log('it was clicked')
+    console.log(add(input1.value, input2.value));
 });
+
+
