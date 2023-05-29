e2c4373811587ff59d971cc3f16452d6eab48418
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-05-30 03:08:54 +0800

    data types

--- app.js
+++
@@ -1,9 +0,0 @@
-var inputNum1 = document.getElementById('input1');
-var inputNum2 = document.getElementById('input2');
-var submitBtn = document.querySelector('button');
-function addNumbers(num1, num2) {
-    return num1 + num2;
-}
-submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener('click', function () {
-    console.log("The sum ".concat(addNumbers(+inputNum1.value, +inputNum2.value)));
-});
