20e51dd4af89325f2888376668fdc8fedfceeefb
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-06-01 18:57:55 +0800

    Done Union Types

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //Union Types
+function addOrCombine(data1, data2) {
+    var result;
+    if (typeof data1 === 'number' && typeof data2 === 'number') {
+        result = data1 + data2;
+    }
+    else {
+        result = data1.toString() + data2.toString();
+    }
+    return result;
+}
+var combineNumbers = addOrCombine(3, 4);
+console.log(combineNumbers);
+var combineString = addOrCombine("Jahzeel ", "Kiel");
+console.log(combineString);
--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
 //Union Types
 
+type PinoyFreeCoder = number | string;
+
+
+function addOrCombine(data1:PinoyFreeCoder, data2:PinoyFreeCoder) {
+    let result : number | string;
+    if (typeof data1 === 'number' && typeof data2 === 'number') {
+        result = data1 + data2;
+    }else{
+        result = data1.toString() + data2.toString();
+    }
+    return result;
+}
+
+
+const combineNumbers = addOrCombine(3, 4);
+console.log(combineNumbers)
+
+const combineString = addOrCombine("Jahzeel ", "Kiel");
+console.log(combineString)
