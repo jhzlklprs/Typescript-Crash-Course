3e8850cea8705a184b3adfc5d8aa22c403f1fc55
Jahzeel Kiel Peras <jhzlklprs@gmail.com>
2023-06-01 18:46:24 +0800

    Union types

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-var Access;
-(function (Access) {
-    Access[Access["ADMIN"] = 0] = "ADMIN";
-    Access[Access["MODERATOR"] = 1] = "MODERATOR";
-    Access[Access["USER"] = 2] = "USER";
-})(Access || (Access = {}));
-var student = {
-    name: 'Juan',
-    age: 19,
-    interest: ["Basketball", "Reading"],
-    access: Access.ADMIN
-};
-if (student.access === 0) {
-    console.log('is admin');
-}
-//1:16:00 tutorial pause
+//Union Types
--- app.ts
+++ app.ts
@@ -1,21 +1,2 @@
-enum Access {ADMIN, MODERATOR, USER}
+//Union Types
 
-const student: {
-    name: string;
-    age: number;
-    gender?: string;
-    interest : string[];
-    access : Access;
-} = {
-    name : 'Juan',
-    age : 19,
-    interest : ["Basketball", "Reading"],
-    access : Access.ADMIN
-}
-
-if(student.access === 0){
-    console.log('is admin')
-}
-
-
-//1:16:00 tutorial pause
